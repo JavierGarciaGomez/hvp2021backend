64512c27acc76fac46f3865dbfb950d1f42b5f35
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-01-16 02:35:53 +0100

    build

--- routes/cleanUpsRoutes.js
+++ routes/cleanUpsRoutes.js
@@ -2,15 +2,15 @@
     host/api/cleanups
 */
 
-const { Router } = require("express");
-const { check } = require("express-validator");
-const {
-  checkCleanUpsAndGenerate,
-  editCleanUp,
-} = require("../controllers/cleanUpsController");
-const { validarJWT } = require("../middlewares/validar-jwt");
-// const { fieldValidator } = require("../middlewares/fieldValidator");
+// const { Router } = require("express");
+// const { check } = require("express-validator");
+// const {
+//   checkCleanUpsAndGenerate,
+//   editCleanUp,
+// } = require("../controllers/cleanUpsController");
 // const { validarJWT } = require("../middlewares/validar-jwt");
+// // const { fieldValidator } = require("../middlewares/fieldValidator");
+// // const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
