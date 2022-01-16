93bb31edf58b0d6160319b4b189081168cd98245
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-01-16 03:15:40 +0100

    build

--- controllers/cleanUpsController.js
+++ controllers/cleanUpsController.js
@@ -7,144 +7,144 @@ const { body } = require("express-validator");
 const DailyCleanup = require("../models/DailyCleanUp");
 const { branches, dailyCleanUpActions } = require("../types/types");
 const { getDateWithoutTime } = require("../helpers/utilities");
-const dayjs = require("dayjs");
+var dayjs = require("dayjs");
 const utc = require("dayjs/plugin/utc");
 const { getCollaboratorById } = require("./collaboratorsController");
 dayjs.extend(utc);
 
-// const checkCleanUpsAndGenerate = async (req, res = response) => {
-//   const date = dayjs().utc(true).startOf("day");
-//   console.log("date", date);
+const checkCleanUpsAndGenerate = async (req, res = response) => {
+  const date = dayjs().utc(true).startOf("day");
+  console.log("date", date);
 
-//   for (branch of branches) {
-//     for (i = 0; i < 7; i++) {
-//       const newDate = date.subtract(i, "day");
-//       // console.log("branch", branch, i);
+  for (branch of branches) {
+    for (i = 0; i < 7; i++) {
+      const newDate = date.subtract(i, "day");
+      // console.log("branch", branch, i);
 
-//       let dailyCleanUp = await DailyCleanup.findOne({ date: newDate, branch });
-//       if (!dailyCleanUp) {
-//         dailyCleanUp = new DailyCleanup({
-//           date: newDate,
-//           branch,
-//         });
-//         const savedDailyCleanUp = await dailyCleanUp.save();
-//       }
-//     }
-//   }
+      let dailyCleanUp = await DailyCleanup.findOne({ date: newDate, branch });
+      if (!dailyCleanUp) {
+        dailyCleanUp = new DailyCleanup({
+          date: newDate,
+          branch,
+        });
+        const savedDailyCleanUp = await dailyCleanUp.save();
+      }
+    }
+  }
 
-//   // TODO:
-//   // return dailycleanups from the last 10 days
+  // TODO:
+  // return dailycleanups from the last 10 days
 
-//   const utcDateEnd = dayjs(date).utc(true).endOf("day");
-//   const utcDateStart = utcDateEnd.subtract(10, "day");
+  const utcDateEnd = dayjs(date).utc(true).endOf("day");
+  const utcDateStart = utcDateEnd.subtract(10, "day");
 
-//   let dailyCleanUps = await DailyCleanup.find({
-//     date: {
-//       $gte: new Date(utcDateStart),
-//       $lt: new Date(utcDateEnd),
-//     },
-//   })
-//     .populate("cleaners.cleaner", "imgUrl col_code")
-//     .populate("supervisors.supervisor", "imgUrl col_code")
-//     .populate("comments.comment", "imgUrl col_code");
+  let dailyCleanUps = await DailyCleanup.find({
+    date: {
+      $gte: new Date(utcDateStart),
+      $lt: new Date(utcDateEnd),
+    },
+  })
+    .populate("cleaners.cleaner", "imgUrl col_code")
+    .populate("supervisors.supervisor", "imgUrl col_code")
+    .populate("comments.comment", "imgUrl col_code");
 
-//   res.json({
-//     ok: true,
-//     msg: "generado",
-//     dailyCleanUps,
-//   });
-// };
+  res.json({
+    ok: true,
+    msg: "generado",
+    dailyCleanUps,
+  });
+};
 
-// const editCleanUp = async (req, res = response) => {
-//   console.log("here");
-//   const { action, comment, cleanUpId } = req.body;
+const editCleanUp = async (req, res = response) => {
+  console.log("here");
+  const { action, comment, cleanUpId } = req.body;
 
-//   const { uid, col_code, role } = req;
+  const { uid, col_code, role } = req;
 
-//   try {
-//     let dailyCleanUp = await DailyCleanup.findById(cleanUpId);
+  try {
+    let dailyCleanUp = await DailyCleanup.findById(cleanUpId);
 
-//     if (!dailyCleanUp) {
-//       return res.status(404).json({
-//         ok: false,
-//         msg: "No existe control de limpieza diario con ese ese id",
-//       });
-//     }
+    if (!dailyCleanUp) {
+      return res.status(404).json({
+        ok: false,
+        msg: "No existe control de limpieza diario con ese ese id",
+      });
+    }
 
-//     // get collaborator
-//     const collaborator = await Collaborator.findById(uid);
+    // get collaborator
+    const collaborator = await Collaborator.findById(uid);
 
-//     let updatedDailyCleanUp;
+    let updatedDailyCleanUp;
 
-//     switch (action) {
-//       case dailyCleanUpActions.addCleaner:
-//         for (element of dailyCleanUp.cleaners) {
-//           if (element.cleaner._id.toString() === uid) {
-//             return res.status(404).json({
-//               ok: false,
-//               msg: "Este colaborador ha sido ya registrado",
-//             });
-//           }
-//         }
-//         // add the cleaner
-//         dailyCleanUp.cleaners.push({ cleaner: collaborator, time: dayjs() });
-//         break;
+    switch (action) {
+      case dailyCleanUpActions.addCleaner:
+        for (element of dailyCleanUp.cleaners) {
+          if (element.cleaner._id.toString() === uid) {
+            return res.status(404).json({
+              ok: false,
+              msg: "Este colaborador ha sido ya registrado",
+            });
+          }
+        }
+        // add the cleaner
+        dailyCleanUp.cleaners.push({ cleaner: collaborator, time: dayjs() });
+        break;
 
-//       case dailyCleanUpActions.addSupervisor:
-//         for (element of dailyCleanUp.supervisors) {
-//           if (element.supervisor._id.toString() === uid) {
-//             return res.status(404).json({
-//               ok: false,
-//               msg: "Este colaborador ha sido ya registrado",
-//             });
-//           }
-//         }
-//         // add the supervisor
-//         dailyCleanUp.supervisors.push({
-//           supervisor: collaborator,
-//           time: dayjs(),
-//         });
+      case dailyCleanUpActions.addSupervisor:
+        for (element of dailyCleanUp.supervisors) {
+          if (element.supervisor._id.toString() === uid) {
+            return res.status(404).json({
+              ok: false,
+              msg: "Este colaborador ha sido ya registrado",
+            });
+          }
+        }
+        // add the supervisor
+        dailyCleanUp.supervisors.push({
+          supervisor: collaborator,
+          time: dayjs(),
+        });
 
-//         console.log("aca maria");
-//         break;
+        console.log("aca maria");
+        break;
 
-//       case dailyCleanUpActions.addComment:
-//         dailyCleanUp.comments.push({ comment, creator: collaborator });
-//         break;
+      case dailyCleanUpActions.addComment:
+        dailyCleanUp.comments.push({ comment, creator: collaborator });
+        break;
 
-//       default:
-//         break;
-//     }
-//     if (action === dailyCleanUpActions.addCleaner) {
-//     }
+      default:
+        break;
+    }
+    if (action === dailyCleanUpActions.addCleaner) {
+    }
 
-//     dailyCleanUp.hasBeenUsed = true;
+    dailyCleanUp.hasBeenUsed = true;
 
-//     console.log("esto se atualizara", dailyCleanUp);
-//     // update it
-//     updatedDailyCleanUp = await DailyCleanup.findByIdAndUpdate(
-//       cleanUpId,
-//       dailyCleanUp,
-//       { new: true }
-//     );
+    console.log("esto se atualizara", dailyCleanUp);
+    // update it
+    updatedDailyCleanUp = await DailyCleanup.findByIdAndUpdate(
+      cleanUpId,
+      dailyCleanUp,
+      { new: true }
+    );
 
-//     console.log(updatedDailyCleanUp);
+    console.log(updatedDailyCleanUp);
 
-//     res.status(201).json({
-//       ok: true,
-//       message: "great",
-//       updatedDailyCleanUp,
-//       uid,
-//       col_code,
-//       role,
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       ok: "false",
-//       msg: "Por favor, hable con el administrador",
-//       error,
-//     });
-//   }
-// };
+    res.status(201).json({
+      ok: true,
+      message: "great",
+      updatedDailyCleanUp,
+      uid,
+      col_code,
+      role,
+    });
+  } catch (error) {
+    res.status(500).json({
+      ok: "false",
+      msg: "Por favor, hable con el administrador",
+      error,
+    });
+  }
+};
 
-// module.exports = { checkCleanUpsAndGenerate, editCleanUp };
+module.exports = { checkCleanUpsAndGenerate, editCleanUp };
