3ff0088eb1ab789d834735e3ce725b511461b2a8
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-01-16 02:46:47 +0100

    build

--- routes/cleanUpsRoutes.js
+++ routes/cleanUpsRoutes.js
@@ -1,28 +1,80 @@
 /* Users routes:
-    host/api/cleanups
+    host/api/collaborator
 */
 
 const { Router } = require("express");
 const { check } = require("express-validator");
 const {
-  checkCleanUpsAndGenerate,
-  editCleanUp,
-} = require("../controllers/cleanUpsController");
+  createCollaborator,
+  getCollaborators,
+  getCollaboratorById,
+  updateCollaborator,
+  registerCollaborator,
+  collaboratorLogin,
+  collaboratorRenewToken,
+} = require("../controllers/collaboratorsController");
+const { fieldValidator } = require("../middlewares/fieldValidator");
+
+// TODO
 const { validarJWT } = require("../middlewares/validar-jwt");
-// const { fieldValidator } = require("../middlewares/fieldValidator");
-// const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
-router.get("/go", (req, res) => {
-  res.send("hola");
-});
+// Login
+router.post(
+  "/",
+  [
+    check("email", "no es una forma de email correcta").isEmail(),
+    fieldValidator,
+  ],
+  collaboratorLogin
+);
 
-// router.get(
-//   "/daily/checkCleanUpsAndGenerate",
-//   validarJWT,
-//   checkCleanUpsAndGenerate
-// );
-// router.patch("/daily/", validarJWT, editCleanUp);
+router.get("/", getCollaborators);
+
+// create collaborator by manager
+router.post(
+  "/create",
+  [
+    check("first_name", "El nombre (s) es obligatorio").not().isEmpty(),
+    check("last_name", "Los apellidos son obligatorios").not().isEmpty(),
+    check(
+      "col_code",
+      "El código de colaborador es obligatorio y debe contener tres letras"
+    ).isLength({ min: 3, max: 3 }),
+
+    fieldValidator,
+  ],
+  createCollaborator
+);
+
+// authenticate collaborator for register
+router.patch(
+  "/register",
+  [
+    check("email", "No es una forma correcta de email").isEmail(),
+    check(
+      "password",
+      "El password es obligatorio y debe contener al menos seis carácteres"
+    ).isLength({ min: 6 }),
+    check(
+      "col_code",
+      "El código de colaborador es obligatorio y debe contener tres letras"
+    ).isLength({ min: 3, max: 3 }),
+    check(
+      "accessCode",
+      "El código de acceso es obligatorio y debe contener 6 carácteres"
+    ).isLength({ min: 6, max: 6 }),
+    fieldValidator,
+  ],
+  registerCollaborator
+);
+
+router.put("/:collaboratorId", updateCollaborator);
+
+// renew token
+router.get("/renew", validarJWT, collaboratorRenewToken);
+
+router.get("/:collaboratorId", getCollaboratorById);
 
 module.exports = router;
