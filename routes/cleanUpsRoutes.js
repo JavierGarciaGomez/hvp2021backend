53d6faac26ae69c6516a763237e210c5b236a0e5
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-01-16 02:37:56 +0100

    build

--- routes/cleanUpsRoutes.js
+++ routes/cleanUpsRoutes.js
@@ -2,23 +2,23 @@
     host/api/cleanups
 */
 
-// const { Router } = require("express");
-// const { check } = require("express-validator");
-// const {
-//   checkCleanUpsAndGenerate,
-//   editCleanUp,
-// } = require("../controllers/cleanUpsController");
+const { Router } = require("express");
+const { check } = require("express-validator");
+const {
+  checkCleanUpsAndGenerate,
+  editCleanUp,
+} = require("../controllers/cleanUpsController");
+const { validarJWT } = require("../middlewares/validar-jwt");
+// const { fieldValidator } = require("../middlewares/fieldValidator");
 // const { validarJWT } = require("../middlewares/validar-jwt");
-// // const { fieldValidator } = require("../middlewares/fieldValidator");
-// // const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
-// router.get(
-//   "/daily/checkCleanUpsAndGenerate",
-//   validarJWT,
-//   checkCleanUpsAndGenerate
-// );
-// router.patch("/daily/", validarJWT, editCleanUp);
+router.get(
+  "/daily/checkCleanUpsAndGenerate",
+  validarJWT,
+  checkCleanUpsAndGenerate
+);
+router.patch("/daily/", validarJWT, editCleanUp);
 
 module.exports = router;
