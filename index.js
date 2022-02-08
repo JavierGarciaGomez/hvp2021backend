7285aa23b865dc28fc78589ffb2199d3bf260a06
Javier Humberto García Gómez <javieron.garcia@gmail.com>
2022-02-08 11:40:00 +0100

    google auth

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ dbConnection();
 app.use(
   cors()
     {
-    origin: process.env.CLIENT_URL,
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
+    // origin: process.env.CLIENT_URL,
+    // methods: "GET,POST,PUT,DELETE",
+    // credentials: true,
   }
 );
 
