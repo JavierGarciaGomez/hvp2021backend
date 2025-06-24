b9a46f314198b654fffc3ab07ef1028535544dae
javiergg <javieron.garcia@gmail.com>
2025-06-24 13:56:11 +0200

    Remove dist folder from git tracking

--- dist/domain/repositories/collaborator.repository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CollaboratorRepository = void 0;
-const base_repository_1 = require("./base.repository");
-class CollaboratorRepository extends base_repository_1.BaseRepository {
-}
-exports.CollaboratorRepository = CollaboratorRepository;
--- dist/pending/routes/collaboratorLogRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const { Router } = require("express");
-const { getLogs } = require("../controllers/collaboratorLogController");
-const { validateJwt } = require("../../presentation/middlewares/validateJwt");
-const router = Router();
-router.get("/", validateJwt, getLogs);
-module.exports = router;
